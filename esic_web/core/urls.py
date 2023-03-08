e592606be4a8c4c45139cf0581767f21db7f2f13
= <jeff.aprentiv@gmail.com>
2023-03-08 16:27:57 +0100

    Ajout des pages actualites  et son contenu , modifications de la page d\acceuil et de certains titres

--- esic_web/core/urls.py
+++ esic_web/core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import home_page, articles_page 
+from .views import home_page, articles_page, actualites_page, presentation_page
 
 
 urlpatterns = [
     #path('home/', include('home.urls')),
     path("", home_page, name="home"),
-    path("/articles", articles_page, name="articles"),
+    path("articles", articles_page, name="articles"),
+    path("actualites", actualites_page, name="actualites"),
+    path("presentation", presentation_page, name="presentation"),
 ]
 
--- esic_web/core/views.py
+++ esic_web/core/views.py
@@ -8,3 +8,10 @@ def home_page(request):
 
 def articles_page(request):
     return render(request, "core/articles.html")
+
+def actualites_page(request):
+    return render(request, "core/actualites.html")
+
+def presentation_page(request):
+    return render(request, "core/presentation.html")
+
